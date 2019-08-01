40e2dee9abb8d6669b7c6993dcd7d0eb6bcdd82b
Lakshamana <guitrompa1@gmail.com>
2019-08-01 16:07:02 -0300

    initial commit

---
+++ src/app/rating.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'mt-rating',
+  templateUrl: './rating.component.html'
+})
+export class RatingComponent {
+  title = 'rating-custom-element'
+}
