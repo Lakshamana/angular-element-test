73224fcac1a05feb6af1f3ec3fbcc560d38587d7
Lakshamana <guitrompa1@gmail.com>
2019-08-01 17:17:19 -0300

    finish component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser'
-import { NgModule } from '@angular/core'
+import { NgModule, Injector } from '@angular/core'
+import { createCustomElement } from '@angular/elements'
 
 import { RatingComponent } from './rating.component'
 
@@ -7,6 +8,13 @@ import { RatingComponent } from './rating.component'
   declarations: [RatingComponent],
   imports: [BrowserModule],
   providers: [],
-  bootstrap: [RatingComponent]
+  entryComponents: [RatingComponent]
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private injector: Injector) {
+    const component = createCustomElement(RatingComponent, { injector })
+    customElements.define('mt-rating', component)
+  }
+
+  ngDoBootstrap () {}
+}
--- src/app/rating.component.ts
+++ src/app/rating.component.ts
@@ -1,9 +1,37 @@
-import { Component } from '@angular/core'
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core'
 
 @Component({
   selector: 'mt-rating',
   templateUrl: './rating.component.html'
 })
-export class RatingComponent {
-  title = 'rating-custom-element'
+export class RatingComponent implements OnInit {
+  rates = [1, 2, 3, 4, 5]
+  tmpRate = 0
+
+  @Input() rate = 0
+  @Output() rated = new EventEmitter<number>()
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  setRate(r: number) {
+    this.rate = r
+    this.tmpRate = undefined
+    this.rated.emit(this.rate)
+  }
+
+  setTmpRate(r: number) {
+    if (!this.tmpRate) {
+      this.tmpRate = this.rate
+    }
+    this.rate = r
+  }
+
+  clearTmpRate() {
+    if (this.tmpRate) {
+      this.rate = this.tmpRate
+      this.tmpRate = undefined
+    }
+  }
 }
